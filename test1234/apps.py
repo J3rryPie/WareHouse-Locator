e91f7c1e1fa232458d735213f5858b325c9b49ef
Aayush Kawathekar <aayush.kawathekar@gmail.com>
2021-04-02 16:16:46 +0530

    new code

--- test1234/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Test1234Config(AppConfig):
-    name = 'test1234'
--- test12354/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Test12354Config(AppConfig):
-    name = 'test12354'
