455139f9526d762f161743a400344bca1485435f
Aayush Kawathekar <aayush.kawathekar@gmail.com>
2021-04-02 15:59:22 +0530

    City Form Updated

---
+++ test1234/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Test1234Config(AppConfig):
+    name = 'test1234'
---
+++ test12354/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Test12354Config(AppConfig):
+    name = 'test12354'
