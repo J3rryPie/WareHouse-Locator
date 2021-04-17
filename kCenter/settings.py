0f9128a96c67c8463bbe3712b33ba7adff3fb90e
gokul5102 <gokulr5102@gmail.com>
2021-04-17 09:21:46 +0530

    heroku

--- kCenter/settings.py
+++ kCenter/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-ECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY=env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG=env('DEBUG')
 
 ALLOWED_HOSTS = ['warehouse-locator.herokuapp.com']
 
